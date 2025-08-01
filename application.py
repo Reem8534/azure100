170dcc188012904572fe6bfab412dd487dd02d6d
Reem  Alzahrani <110042258+Reem8534@users.noreply.github.com>
2025-08-02 01:56:23 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,10 @@
-"""
-This script runs the FlaskWebProject application using a development server.
-"""
-
 from os import environ
 from FlaskWebProject import app
 import logging
 
 if __name__ == '__main__':
     app.debug = True
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')  # Change default to 0.0.0.0
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
