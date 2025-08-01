e22143f08def0d8a813de3ed4d0f6b07df0a9b0d
Reem  Alzahrani <110042258+Reem8534@users.noreply.github.com>
2025-08-02 01:59:20 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
+"""
+This script runs the FlaskWebProject application using a development server.
+"""
+
 from os import environ
 from FlaskWebProject import app
 import logging
 
 if __name__ == '__main__':
     app.debug = True
-    HOST = environ.get('SERVER_HOST', '0.0.0.0')  # Change default to 0.0.0.0
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')  # Use 0.0.0.0 to accept external connections
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+    # Run with HTTPS using adhoc self-signed cert
+    app.run(host=HOST, port=PORT, ssl_context='adhoc')
